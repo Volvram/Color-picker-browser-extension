37e07b9180647b57df7461f0841578d62444bf83
Julia <yuliabobrovich@yandex.ru>
2025-06-25 23:27:51 +0300

    Added extension with color picker and button

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,11 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['node_modules', 'dist', 'public']),
+  eslintConfigPrettier,
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -19,5 +21,13 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        {allowConstantExport: true},
+      ],
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-console": 1, // 1 - means warning; 2 - means error
+    }
   },
 ])
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState<string>('#08012D');
+
+  const onClick = async () => {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+    });
+    chrome.scripting.executeScript({
+      target: {
+        tabId: tab.id!,
+      },
+      args: [color],
+      func: (color) => {
+        document.body.style.backgroundColor = color;
+      },
+    });
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vite.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className='card'>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+          <input
+            type={'color'}
+            defaultValue={'#08012D'}
+            value={color}
+            onChange={(e) => setColor(e.currentTarget.value)}
+          />
+          <button onClick={onClick}>Set color</button>
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
